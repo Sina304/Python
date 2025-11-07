e0d98d463b18e6bada2db9907f6cd0e9ec9822ab
Sina304 <sinalol1744@gmail.com>
2025-11-07 13:14:40 +0100

    adding_comments

--- lab3/1.py
+++ lab3/1.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, asdict
 import json
 
-@dataclass
+@dataclass   #dzieki temu mamy automatyucznie generowany __init__ oraz mozemy wygodnie sobie wypisac dane
 class Person:
 
     first_name: str
@@ -13,14 +13,14 @@ class Person:
 
     def save_to_json(self, filename: str):
         with open(filename, 'w', encoding='utf-8') as f:
-            json.dump(asdict(self), f, ensure_ascii=False, indent=4)
-
+            json.dump(asdict(self), f, ensure_ascii=False, indent=4)    #asdict konwertuje obiekt do slownika
+                                                            #ensure_ascii=false pozwala na polskie znaki
     @classmethod
     def load_from_json(cls, filename: str):
         with open(filename, 'r', encoding='utf-8') as f:
             data = json.load(f)
-        return cls(**data)
-
+        return cls(**data)              #tworzy obiekt z danych ** powoduje ze rozpakowuje slownik na do argumentow
+                                        #* dla list; ** dla slownikow
 # test
 p = Person("Jan", "Kowalski", 25, 12233256845, "03-255", "ul. Dąbrowskiego 10, Kraków")
 p.save_to_json("person.json")
